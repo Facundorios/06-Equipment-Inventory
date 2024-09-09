3062de831f5bc13a955364f976cb67c646452e9b
Facundo Ríos <Facundorios2005@gmail.com>
2024-09-09 13:50:15 -0300

    feat: :fire: Se añadió conexión a la base de datos y creación del proyecto frontend
    se creó la conexión a la abse de datos usando postgreSQL, y variables de entorno .env. Del lado del frontend, se añadieron el proyecto inicial de vite con React.

--- server/src/Server.ts
+++ server/src/Server.ts
@@ -2,6 +2,19 @@ import express, { Application } from "express";
 import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
+import dotenv from "dotenv";
+
+import { Pool } from "pg";
+dotenv.config();
+
+import {
+  HOST,
+  DB_NAME,
+  DB_PASS,
+  PORT,
+  DB_PORT,
+  DB_USER,
+} from "./configuration/env/enviroments";
 
 import equipmentRoutes from "./routes/equipment.routes";
 
@@ -11,23 +24,39 @@ export class Server {
   private port: number;
   private host: string;
 
+  //pool: es una conexión a la base de datos
+  private pool: Pool;
+
   //contructor: se ejecuta cuando se CREA una instancia de la clase
   constructor() {
     //this: hace referencia a la misma clase
     this.app = express();
-    this.port = 3000;
+    this.port = Number(PORT);
     this.host = "localhost";
+    this.pool = new Pool({
+      host: HOST,
+      port: Number(DB_PORT),
+      user: DB_USER,
+      password: DB_PASS,
+      database: DB_NAME,
+    });
 
     this.database();
     this.middlewares();
     this.routes();
   }
 
-  //void: no devuelve nada, solo ejecuta una acción (no retorna nada)
-  private database(): void {
-    console.table(["MongoDB", "Connected"]);
+  //void: no devuelve nada, solo ejecuta una acción (no retorna nada), con el Promise<void> se indica que es una promesa que no retorna nada
+  private async database(): Promise<void> {
+    try {
+      await this.pool.connect();
+      console.log("PostgreSQL connected successfully");
+    } catch (error) {
+      console.error("Database connection error:", error);
+    }
   }
 
+  //configuración de middlewares
   private middlewares(): void {
     this.app.use(cors());
     this.app.use(morgan("dev"));
---
+++ server/src/configuration/env/enviroments.ts
@@ -0,0 +1,6 @@
+export const PORT: number = Number(process.env.PORT);
+export const HOST: string = process.env.HOST || "localhost";
+export const DB_USER: string = process.env.DB_USER || "postgres";
+export const DB_PASS: string = process.env.DB_PASS || "password";
+export const DB_NAME: string = process.env.DB_NAME || "formotex_db";
+export const DB_PORT: number = Number(process.env.DB_PORT) || 5432;
